c5726f85c3bcb1f568e373b20b8ac3c661c998a5
Ben Altman <altmanguitar@gmail.com>
2020-11-27 05:02:34 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,12 +2,18 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
-  BRANDS = []
-
+  BRANDS = []         #create class constant, BRANDS
+=begin
   def initialize(brand)
     @brand = brand
-    BRANDS << brand
-    Shoe::BRANDS.uniq!
+    BRANDS << brand   #shovel brands into Shoe::BRANDS
+    Shoe::BRANDS.uniq!  #remove duplicates
+  end
+=end
+  def initialize(brand)
+    @brand = brand
+    if !BRANDS.include brand
+    BRANDS << brand   
   end
 
   def cobble
